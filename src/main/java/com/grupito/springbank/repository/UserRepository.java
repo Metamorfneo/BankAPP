924b4f690b233db273a27d1554ed204ffcd5ecba
Samuel <sblazquez2002@gmail.com>
2024-10-20 18:56:47 +0200

    Creamos el Repositorio de Usuario

---
+++ src/main/java/com/grupito/springbank/repository/UserRepository.java
@@ -0,0 +1,8 @@
+package com.grupito.springbank.repository;
+
+import com.grupito.springbank.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User , Long> {
+
+}
