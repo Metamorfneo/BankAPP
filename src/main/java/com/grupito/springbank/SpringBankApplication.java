f9b2b9f0c841e9ff9e04d179b94e93cd214529ed
samuelblazquezdiaz <sblazquez2002@gmail.com>
2025-01-01 14:32:11 +0100

    Empezamos a instaurar Swagger en el sistema para tener todo documentado , lo dejamos un poco apartado para terminar lo importante

--- src/main/java/com/grupito/springbank/SpringBankApplication.java
+++ src/main/java/com/grupito/springbank/SpringBankApplication.java
@@ -1,9 +1,35 @@
 package com.grupito.springbank;
 
+import io.swagger.v3.oas.annotations.ExternalDocumentation;
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+        info = @Info(
+                title = "BankAppSam",
+                description = "Trabajo de final de grado",
+                version = "0.9",
+                contact = @Contact(
+                        name = "Samuel Blazquez Diaz",
+                        email = "sblazquez2002@gmail.com",
+                        url = "https://github.com/Metamorfneo/BankAPP"
+                ),
+                license = @License(
+                        name = "Samuel Blazquez diaz",
+                        url = "https://github.com/Metamorfneo/BankAPP"
+                )
+        ),
+        externalDocs = @ExternalDocumentation(
+                description = "TFG Samuel Blazquez",
+                url = "https://github.com/Metamorfneo/BankAPP"
+        )
+
+)
 public class SpringBankApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/grupito/springbank/controller/UserController.java
+++ src/main/java/com/grupito/springbank/controller/UserController.java
@@ -3,21 +3,32 @@ package com.grupito.springbank.controller;
 
 import com.grupito.springbank.dto.*;
 import com.grupito.springbank.service.impl.UserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/user")
+@Tag(name = "Controlador de APIs")
 public class UserController {
 
     @Autowired
     UserService userService;
-
+    @Operation(
+            summary = "Crear Nueva Cuenta",
+            description = "Creamos una nueva cuenta y le asignamos un numero ID"
+    )
     @PostMapping
     public BankResponse createAccount(@RequestBody UserRequest userRequest) {
         return userService.createAccount(userRequest);
     }
-
+    @Operation(
+            summary = "Consulta de saldo",
+            description = "Damos un numero de cuenta , comprobamos cuanto dinero hay en la cuenta"
+    )
     @GetMapping("balanceEnquiry")
     public BankResponse BalanceEnquiry(@RequestBody EnquiryRequest request) {
         return userService.balanceEnquiry(request);
