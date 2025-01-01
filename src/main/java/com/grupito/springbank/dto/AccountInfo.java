5abf85a30d504495c20d7bdc1e5f86f6274a8850
samuelblazquezdiaz <sblazquez2002@gmail.com>
2025-01-01 14:38:40 +0100

    Metemos la dependencia para Generar documentacion en PDF

--- src/main/java/com/grupito/springbank/dto/AccountInfo.java
+++ src/main/java/com/grupito/springbank/dto/AccountInfo.java
@@ -1,5 +1,6 @@
 package com.grupito.springbank.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,7 +13,16 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 @NoArgsConstructor
 public class AccountInfo {
+    @Schema(
+            name = "Nombre de la cuenta"
+    )
     private String accountName;
+    @Schema(
+            name = "Balance de la cuenta"
+    )
     private BigDecimal accountBalance;
+    @Schema(
+            name = "Numero de la cuenta"
+    )
     private String accountNumber;
 }
